39c616bb14c51df8694868dbde32cd5d91ef32d8
Ismael Silva <ismaelgomesesilva@gmail.com>
2025-08-01 15:53:08 -0300

    fix PhpCsFixer calling

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -3,9 +3,8 @@
 $finder = PhpCsFixer\Finder::create()
     ->in(__DIR__ . '/src');
 
-return PhpCsFixer\Config::create()
+return (new PhpCsFixer\Config())
     ->setRules([
         '@PSR12' => true,
-        'array_syntax' => ['syntax' => 'short'],
     ])
     ->setFinder($finder);
